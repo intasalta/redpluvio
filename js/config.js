7f3585eaf83b0a12c1e81157095d6ed39c9c23a6
intasalta <rrnn.eeasalta@gmail.com>
2026-09-04 17:56:02 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
 const CONFIG = {
-    // Cambiar por tu URL de producción en Render/Railway al desplegar el backend Python
-    API_BASE_URL: 'http://127.0.0.1:8000/api'
+    API_BASE_URL: 'https://redpluvio-backend.onrender.com/api'
 };
